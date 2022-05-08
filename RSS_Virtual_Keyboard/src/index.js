57d84b6b3b0e0fd9583c0774b3db7a61f7d82c70
Ewdoha <olexiyewdoshchenko@gmail.com>
2022-05-08 19:05:32 +0300

    feat: add function createKeyboardContainer, createTextArea, createKeysContainer

--- RSS_Virtual_Keyboard/src/index.js
+++ RSS_Virtual_Keyboard/src/index.js
@@ -1,2 +1,28 @@
 import "./assets/styles/css.css";
 import "./assets/styles/style.scss";
+
+const body = document.querySelector('body');
+
+function createKeyboardContainer () {
+  const keyboardContainer = document.createElement('div');
+  keyboardContainer.className = 'keyboard-container';
+  body.appendChild(keyboardContainer);
+  createTextArea (keyboardContainer);
+  createKeysContainer (keyboardContainer);
+}
+
+function createTextArea (keyboardContainer) {
+  const textArea = document.createElement('textarea');
+  textArea.className = 'textarea';
+  keyboardContainer.appendChild(textArea);
+}
+
+function createKeysContainer (keyboardContainer) {
+  const keysContainer = document.createElement('div');
+  keysContainer.className = 'keys-container';
+  keyboardContainer.appendChild(keysContainer);
+}
+
+
+
+createKeyboardContainer ();

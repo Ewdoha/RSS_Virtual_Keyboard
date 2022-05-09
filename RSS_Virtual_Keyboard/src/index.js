8483b2e3ff80dac5a55d2889f9df9ce004ee5145
Ewdoha <olexiyewdoshchenko@gmail.com>
2022-05-09 19:00:21 +0300

    feat: add eventListener for key Enter

--- RSS_Virtual_Keyboard/src/index.js
+++ RSS_Virtual_Keyboard/src/index.js
@@ -109,113 +109,51 @@ const keysContainer = document.createElement('div');
     || event.target.textContent === 'top' || event.target.textContent === 'left' || event.target.textContent === 'right' || event.target.textContent === 'bottom') {
       false;
     } else {  
-      textAreaText += event.target.innerText;
-      textArea.innerText = textAreaText;  
+      if (capsFlag) {
+        textAreaText += event.target.innerText.toUpperCase();
+        textArea.value = textAreaText;
+      } else {
+        textAreaText += event.target.innerText;
+        textArea.value = textAreaText;
+      }  
     }
   }));
   
   keyBackspace.addEventListener('click', function () {
     textAreaText = textAreaText.slice(0, -1);
-    textArea.innerText = textAreaText;
+    textArea.value = textAreaText;
   })
 
   keyTab.addEventListener('click', function () {
     textAreaText = textAreaText.concat('        ');
-    textArea.innerText = textAreaText;
+    textArea.value = textAreaText;
+  })
+
+  keyCapsLock.addEventListener('click', function () {
+    console.log(capsFlag);
+    //valueKeysRow1 = valueKeysRow1Caps;
+    //keysArray.forEach(el => el.remove());
+    if (capsFlag) {  
+      capsFlag = false;  
+    } else { 
+      capsFlag = true;  
+    } 
+  }) 
+ 
+  keyEnter.addEventListener('click', function () {
+    textAreaText += '\r\n';
+    textArea.value = textAreaText;
   })
 
   
-  keyCapsLock.addEventListener('click', function () {
-    console.log(capsFlag);
+//console.log(textArea.value);
 
-    if (capsFlag === false) {
-      keysArray.forEach(el => el.remove());
+/*  const end = textArea.value.length;
 
-      for (let i = 0; i < valueKeysRow1Caps.length; i++) {
-        let keyRow1 = document.createElement('div');
-        keyRow1.className = 'key keyRow-1';
-        keyRow1.textContent = valueKeysRow1Caps[i];
-        keysContainer.appendChild(keyRow1);
-      }
+    textArea.setSelectionRange(end, end);
+    textArea.focus();
     
-      for (let i = 0; i < valueKeysRow2Caps.length; i++) {
-        let keyRow2 = document.createElement('div');
-        keyRow2.className = 'key keyRow-2';
-        keyRow2.textContent = valueKeysRow2Caps[i];
-        keysContainer.appendChild(keyRow2);
-      }
-    
-      for (let i = 0; i < valueKeysRow3Caps.length; i++) {
-        let keyRow3= document.createElement('div');
-        keyRow3.className = 'key keyRow-3';
-        keyRow3.textContent = valueKeysRow3Caps[i];
-        keysContainer.appendChild(keyRow3);
-      }
-    
-      for (let i = 0; i < valueKeysRow4Caps.length; i++) {
-        let keyRow4 = document.createElement('div');
-        keyRow4.className = 'key keyRow-4';
-        keyRow4.textContent = valueKeysRow4Caps[i];
-        keysContainer.appendChild(keyRow4);
-      }
-    
-      for (let i = 0; i < valueKeysRow5Caps.length; i++) {
-        let keyRow5 = document.createElement('div');
-        keyRow5.className = 'key keyRow-5';
-        keyRow5.textContent = valueKeysRow5Caps[i];
-        keysContainer.appendChild(keyRow5);
-      }
-
-      capsFlag = true;
-      
-    } else if (capsFlag === true){
-
-      keysArray.forEach(el => el.remove());
-
-      for (let i = 0; i < valueKeysRow1.length; i++) {
-        let keyRow1 = document.createElement('div');
-        keyRow1.className = 'key keyRow-1';
-        keyRow1.textContent = valueKeysRow1[i];
-        keysContainer.appendChild(keyRow1);
-      }
-    
-      for (let i = 0; i < valueKeysRow2.length; i++) {
-        let keyRow2 = document.createElement('div');
-        keyRow2.className = 'key keyRow-2';
-        keyRow2.textContent = valueKeysRow2[i];
-        keysContainer.appendChild(keyRow2);
-      }
-    
-      for (let i = 0; i < valueKeysRow3.length; i++) {
-        let keyRow3= document.createElement('div');
-        keyRow3.className = 'key keyRow-3';
-        keyRow3.textContent = valueKeysRow3[i];
-        keysContainer.appendChild(keyRow3);
-      }
-    
-      for (let i = 0; i < valueKeysRow4.length; i++) {
-        let keyRow4 = document.createElement('div');
-        keyRow4.className = 'key keyRow-4';
-        keyRow4.textContent = valueKeysRow4[i];
-        keysContainer.appendChild(keyRow4);
-      }
-    
-      for (let i = 0; i < valueKeysRow5.length; i++) {
-        let keyRow5 = document.createElement('div');
-        keyRow5.className = 'key keyRow-5';
-        keyRow5.textContent = valueKeysRow5[i];
-        keysContainer.appendChild(keyRow5);
-      }
-
-      capsFlag = false;
-    } else {
-      false;
-    }
-
-   
-  })
-
-//console.log(capsFlag);
-
+     textArea.focus(); 
+    textArea.moveStart();*/
 
   
